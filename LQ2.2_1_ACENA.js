f11492a397379548a9e717f7fbd21cc4778b5b52
Acena, Gary Dave <daveramirez200408@gmail.com>
2025-03-27 11:48:04 +0800

    Add files via upload

---
+++ LQ2.2_1_ACENA.js
@@ -0,0 +1,11 @@
+// Prompt user for username and password
+let userName = prompt("Enter your username:");
+let userPassword = prompt("Enter your password:");
+let correctPassword = "password123";
+
+// Check if the username and password are correct
+if (userPassword === correctPassword) {
+    alert(`Welcome ${userName}`);
+} else {
+    alert("Maybe Username or Password is Invalid or Does not Match");
+}
