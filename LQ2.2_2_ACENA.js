56aa2bc95f24001b36392744b18382dc27dc8b84
Acena, Gary Dave <daveramirez200408@gmail.com>
2025-03-28 16:09:14 +0800

    Delete LQ2.2_2_ACENA.js

--- LQ2.2_2_ACENA.js
+++
@@ -1,8 +0,0 @@
-// Propmt the user to enter the breed of their dog
-let dog_breed = prompt("Enter the breed of your dog: ");
-let print_count = parseInt(prompt("Enter the number of times you want to print the breed: "));
-
-// Print the breed of the dog the number of times the user entered
-for (let i = 0 ; i < print_count ; i++) {
-    console.log(dog_breed);
-}
